ec545c07794b40f97bb06a7dae541b738cf19b67
vaibhavgowda23 <devopsnhc68@gmail.com>
2023-03-01 20:55:14 -0800

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
         <div className="footer-content">
           <div className="footer-section about">
             <p className="footer-para">
-              Developer: <span className="my-name"> Tadawvkugwviywrvuvwbi</span>
+              Developer: <span className="my-name"> Junaid Qureshi</span>
             </p>
 
             <div className="contact"></div>
@@ -44,7 +44,7 @@ function Footer() {
               </a>
             </div>
             <p className="copyright">
-              Copyright&nbsp;&copy;&nbsp;Nsdvsdike Shoes Temdsvdvsdsvsplate
+              Copyright&nbsp;&copy;&nbsp;© Nike Shoes Template
             </p>
             <br />
           </div>
