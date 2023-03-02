9b5f84d96e35b8944fcb5ebafbe06c787dfeda87
vaibhavgowda23 <devopsnhc68@gmail.com>
2023-03-01 20:25:15 -0800

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,7 @@ function Home() {
     <div>
       <div className="banner-image"></div>
       <div>
-        <span className="tagline">JUST DOES IT</span>
+        <span className="tagline">JUST DO IT</span>
         <div className="tagline-para">
           Nike has been your compadsdvsdvnion in happiness, both big and small, for
           over 50 years. Our roots are firmly grounded in India and with its
